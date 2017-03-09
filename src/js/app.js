1f780af23f893539db0e0b0821da651fc2f89e0b
Dawid Jaracz <Dawid Jaracz>
2017-03-09 19:57:03 +0100

    loading page created

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,22 @@
-console.log('works');
-console.log('works');
+$(document).ready(function () {
+
+  var loadingPage = (function () {
+    var loadingPage = $('#loading-page');
+
+    var removeLoadingContent = function () {
+      loadingPage.remove();
+    };
+
+    var hideLoadingContent = function () {
+      loadingPage.delay(1500).fadeOut('slow', removeLoadingContent)
+    };
+
+    return {
+      hideLoadingContent: hideLoadingContent,
+    };
+
+  })();
+
+  loadingPage.hideLoadingContent();
+
+});
