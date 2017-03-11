ef318841c3369c273f482ab1ac003fc9101342c3
Dawid Jaracz <Dawid Jaracz>
2017-03-11 01:40:01 +0100

    basic drawer complete

--- src/js/drawer.js
+++ src/js/drawer.js
@@ -2,42 +2,62 @@ $(document).ready(function () {
 
   const toggleButton = (function () {
 
+    let drawer = $('.drawer');
     let toggleBtn = $('i#toggle');
+    let showDrawerStyle = 'show-drawer';
     let hamburgerStyle = 'fa-bars';
     let backArrowStyle = 'fa-arrow-left';
+    let showDrawer = false;
 
     let switchToggle = () => {
       let checkIfIconIsHamburger = toggleBtn.is('.fa-bars');
-      console.log(checkIfIconIsHamburger);
 
       if(checkIfIconIsHamburger) {
+        showDrawer = true;
         switchToBackArrow();
+        drawerSupport();
       } else {
-        switchToHamburger();
+        showDrawer = false;
+        drawerToggleHideAnimation();
+        drawerSupport();
       }
     };
 
-    let switchToHamburger = () => {
-      toggleBtn.animate({
-        left: '-=170'
+    let drawerSupport = () => {
+      if (showDrawer) {
+        drawer.addClass(showDrawerStyle);
+      } else {
+        drawerHideAnimation();
+      }
+    };
+
+    let drawerHideAnimation = () => {
+      drawer.animate({
+        left: '-=270'
       }, 600, () => {
-        toggleBtn.css('left', () => {
-          return 30;
-        })
+        drawer.css('left', () => 0);
+        drawer.removeClass(showDrawerStyle);
+      })
+    };
+
+    let drawerToggleHideAnimation = () => {
+      toggleBtn.animate({
+        left: '-=270'
+      }, 600, () => {
+        toggleBtn.css('left', () => 30);
         switchIcon(hamburgerStyle);
       });
-    }
+    };
 
     let switchToBackArrow = () => {
       switchIcon(backArrowStyle);
-    }
+    };
 
     let switchIcon = (newStyle) => {
       let styleToRemove = (newStyle === hamburgerStyle) ? backArrowStyle : hamburgerStyle;
 
       toggleBtn.removeClass(styleToRemove)
-              .addClass(newStyle)
-              .fadeIn();
+              .addClass(newStyle);
     };
 
     let mountToggle = () => {
--- src/js/loadingPage.js
+++ src/js/loadingPage.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
   const loadingPage = (function () {
     let loadingPage = $('#loading-page');
 
-    let removeLoadingContent = function () {
+    let removeLoadingContent = () => {
       loadingPage.remove();
     };
 
-    let hideLoadingContent = function () {
+    let hideLoadingContent = () => {
       loadingPage.delay(1500).fadeOut('slow', removeLoadingContent)
     };
 
